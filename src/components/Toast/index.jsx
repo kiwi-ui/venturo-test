38d6ce19bc77dcbeff1a4c6553521d73107a5604
kiwi-ui <yusnianggara@gmail.com>
2023-09-14 12:39:22 +0700

    add voucher feature

--- src/components/Toast/index.jsx
+++ src/components/Toast/index.jsx
@@ -1,23 +1,30 @@
 import { useEffect, useState } from "react";
 export const Toast = ({HandleToggleToast, cart, updateCartItemQuantity}) => {
+    const [selectedVoucher, setSelectedVoucher] = useState(null);
     const [total, setTotal] = useState(0);
 
     useEffect(() => {
-        // Calculate the total cost
-        const newTotal = cart.reduce((acc, item) => {
-          return acc + item.quantity * item.harga;
-        }, 0);
-      
-        // Update the total state
-        setTotal(newTotal);
-      }, [cart]);
+    let totalWithVoucher = cart.reduce((acc, item) => {
+        return acc + item.quantity * item.harga;
+      }, 0);
+  
+      if (selectedVoucher === 'hemat') {
+        totalWithVoucher -= 10000; 
+      } else if (selectedVoucher === 'puas') {
+        totalWithVoucher -= 100000;
+      }
+  
+      totalWithVoucher = Math.max(0, totalWithVoucher);
+  
+      setTotal(totalWithVoucher);
+    }, [cart, selectedVoucher]);
       
     const incrementQuantity = (itemId) => {
-        updateCartItemQuantity(itemId, 1); // Increase quantity by 1
+        updateCartItemQuantity(itemId, 1); 
       };
       
       const decrementQuantity = (itemId) => {
-        updateCartItemQuantity(itemId, -1); // Decrease quantity by 1
+        updateCartItemQuantity(itemId, -1); 
       };
       
       
@@ -48,10 +55,15 @@ export const Toast = ({HandleToggleToast, cart, updateCartItemQuantity}) => {
                     </div>        
                 ))
             }
-        <div className="d-flex flex-row bg-dark-subtle justify-content-between ">
-            <p>Total</p>
-            <p>{total}</p>
-        </div>
+            <div className="voucher-options">
+                <button onClick={() => setSelectedVoucher('hemat')}>Hemat (Rp 10.000)</button>
+                <button onClick={() => setSelectedVoucher('puas')}>Puas (Rp 100.000)</button>
+            </div>
+
+            <div className="d-flex flex-row bg-dark-subtle justify-content-between ">
+                <p>Total</p>
+                <p>{total}</p>
+            </div>
         </div>
 
     </section>
